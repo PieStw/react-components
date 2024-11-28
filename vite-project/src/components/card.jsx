db529811fd8dde9ad99b5e3deb016c73860ee3f6
PieStw <piero.stalteri@icloud.com>
2024-11-28 14:42:04 +0100

    aggiunto card e css

--- vite-project/src/components/card.jsx
+++ vite-project/src/components/card.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import img from "../assets/img.jpg";
+import "./card.css";
 
 export default function Card() {
   return (
     <>
       <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
+        <img src={img} className="card-img-top" />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Titolo del post</h5>
           <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam vero,
+            consequatur nisi optio quidem quae dignissimos quibusdam ab dicta
+            voluptatibus perspiciatis, error, cumque quod distinctio nostrum
+            ratione earum deleniti illo!
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
+          <a href="#" className="btn btn-warning">
+            Leggi di più
           </a>
         </div>
       </div>
