348154eb976eb7540c07fa50691ea16117573779
PieStw <piero.stalteri@icloud.com>
2024-11-28 14:42:18 +0100

    aggiunta immagine

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Card from "./components/card";
+import Header from "./components/header";
+import Body from "./components/body";
 import Footer from "./components/footer";
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
 
   return (
     <>
-      <Card />
+      <Header />
+      <Body />
       <Footer />
     </>
   );
